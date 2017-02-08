45bdcd3d3ba28b3dc01ec3cc98040b45d8bfa500
Aare-Johannes Vartšun <aare-johannes.vartsun@vykk.lan>
2017-02-08 10:31:05 +0200

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+
+namespace Pidu
+{
+
+    public sealed partial class MainPage : Page
+    {
+        Korter korter1 = new Korter();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void lisaButton_Click(object sender, RoutedEventArgs e)
+        {
+            /*  List<string> massiiv = new List<string>();
+              massiiv.Add("mari");
+              massiiv.Add("kalle");
+              string tekst = "";
+              for (int i = 0; i < massiiv.Count; i++)
+              {
+                  tekst = tekst + massiiv[i].ToString() + " ∙ "; 
+              }
+              textblock1.Text = tekst; */
+            if (!string.IsNullOrWhiteSpace(textbox1.Text))
+            {
+            korter1.lisaInimene(textbox1.Text);
+            textbox1.Text = "";
+            textblock1.Text = korter1.kuvaInimesed();
+            }
+            else
+            {
+                var dialog = new MessageDialog("Sul on nimi ka ju raibe!");
+                dialog.ShowAsync();
+            }
+        }
+
+        private void eemaldaButton_Click(object sender, RoutedEventArgs e)
+        {
+            korter1.eemaldaInimene(textbox1.Text);
+            textbox1.Text = "";
+            textblock1.Text = korter1.kuvaInimesed();
+        }
+    }
+}
